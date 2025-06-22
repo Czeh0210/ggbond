6fd6a9ae53f7274442dcaf7bcbc9834d38327672
chuiitan0210@gmail.com <chuiitan0210>
2025-06-22 08:31:36 +0800

    Profile adjusting

--- app/components/ReturnMap.tsx
+++ app/components/ReturnMap.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function ReturnMap() {
   return (
-    <Link href="/map">
+    <Link href="/exploration">
       <div className="return-map-container relative group">
         <img 
           src="/return-map.png" 
